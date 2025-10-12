ccb659ad314c1b4997044bf1cdc5ad5dd28875de
clumsyninja007 <clumsyninja007@archerharmony.com>
2025-10-12 18:29:22 -0400

    add LinkCard component and update contact section in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { ref, onMounted, watch } from 'vue'
-import ToggleSwitch from 'primevue/toggleswitch'
+import { ref, onMounted, watch } from 'vue';
+import ToggleSwitch from 'primevue/toggleswitch';
+import Divider from 'primevue/divider';
+import Chip from 'primevue/chip';
+import LinkCard from "@/components/LinkCard.vue";
 
 const isDark = ref<boolean>(false)
 
@@ -41,9 +44,24 @@ function updateTheme() {
     <h2 class="text-4xl mt-4 font-light uppercase text-gray-100">Senior Software Engineer</h2>
   </header>
   <main class="flex-1 flex justify-center">
-    <div class="max-w-6xl w-full flex">
-      <div class="bg-surface-300 dark:bg-surface-800 flex-1 p-8">
+    <div class="max-w-6xl w-full flex shadow-md shadow-surface-300 dark:shadow-surface-900">
+      <div class="bg-surface-300 dark:bg-surface-700 flex-1 p-8">
         <h3 class="text-5xl font-bold">Contact</h3>
+        <div class="flex flex-col gap-3 mt-4">
+          <Chip label="+1 707-601-9915" icon="pi pi-phone" />
+          <LinkCard
+            label="archerharmony@proton.me"
+            link="mailto:archerharmony@proton.me"
+            icon="pi-envelope"
+          />
+          <LinkCard
+            label="LinkedIn"
+            link="https://linkedin.com/in/archerharmony"
+            icon="pi-linkedin"
+          />
+        </div>
+
+        <Divider />
       </div>
       <div class="bg-surface-200 dark:bg-surface-600 flex-2 p-8">
         <h3 class="text-5xl font-bold">Work Experience</h3>
---
+++ src/components/LinkCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Chip from "primevue/chip";
+
+interface LinkCardProps {
+  label: string;
+  link: string;
+  target?: string;
+  icon: string;
+}
+
+const props = defineProps<LinkCardProps>();
+</script>
+
+<template>
+  <a :href="props.link" class="no-underline block" target="_blank" rel="noreferrer">
+    <Chip
+      :label="props.label"
+      :icon="'pi ' + props.icon"
+      :pt="{ root: { class: 'w-full' } }"
+    />
+  </a>
+</template>
+
+<style scoped>
+a:hover :deep(.p-chip-label) {
+  text-decoration: underline;
+}
+</style>
