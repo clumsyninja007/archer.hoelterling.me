07b80ec3f1b7346efc04ce27f24101902293a569
clumsyninja007 <clumsyninja007@archerharmony.com>
2025-10-15 00:23:52 -0400

    undo last commit

--- src/api/http.ts
+++ src/api/http.ts
@@ -4,7 +4,6 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    'Referrer-Policy': 'strict-origin-when-cross-origin',
   },
 })
 
